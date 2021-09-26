0c8a3fc085235234b18ebe34691e374cd9ed1907
thehightech42 <thehightech42@gmail.com>
2021-09-26 05:28:47 +0200

    Add fontionnality to update user information and password

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,10 +4,17 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationType;
+use Symfony\Component\Form\FormError;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
@@ -48,6 +55,72 @@ class SecurityController extends AbstractController
         ]);
 
     }
+
+    /**
+     * @Route("/mon-compte", name="security_myaccount")
+     */
+    public function myaccount(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder): Response
+    {
+        $user = $this->getUser();
+
+        //Form user information
+        $formUser = $this->createFormBuilder($user)
+                        ->add('username', TextType::class)
+                        ->add('email', EmailType::class)
+                        ->add('save', SubmitType::class)
+                        ->getForm(); 
+
+        $formUser->handleRequest($request);
+        
+        if($formUser->isSubmitted() && $formUser->isValid() ){
+            $manager->persist($user); 
+            $manager->flush();
+
+        }
+
+        //Form update user password
+        $formPassword = $this->createFormBuilder($user)
+                        ->add('username', HiddenType::class)
+                        ->add('email', HiddenType::class)
+                        ->add('oldPassword', PasswordType::class)
+                        ->add('newPassword', RepeatedType::class, [
+                            'type'=>PasswordType::class,
+                            'invalid_message'=> "Les mots de saisis ne sont pas les mêmes", 
+                            'options'=>['attr' => ['class'=>'password-field']],
+                            'required'=>true,
+                            'first_options'=>['label'=>'Mot de passe', 'attr' => ['placeholder'=>'Votre mot de passe']],
+                            'second_options'=>['label'=>'Confirmation Mot de passe', 'attr' => ['placeholder'=>'Confirmer votre mot de passe']]
+                        ])
+                        ->add('save', SubmitType::class)
+                        ->getForm(); 
+
+        $formPassword->handleRequest($request);
+        $oldPasswordCheck = true;
+
+        if($formPassword->isSubmitted() && $formPassword->isValid()){
+            var_dump($user);
+            if($encoder->isPasswordValid($user, $user->oldPassword)){
+                $newPasswordHash = $encoder->encodePassword($user, $user->newPassword);
+                $user->setPassword($newPasswordHash); 
+
+                $manager->persist($user);
+                $manager->flush();
+            }else{
+                var_dump('false oldPassword');
+                //$oldPasswordCheck = false;
+                $formPassword->get('oldPassword')->addError(new FormError('Votre ancien mot de passe ne correspond pas'));
+            }
+        }
+
+
+        return $this->render('security/myaccount.html.twig', [
+            'formUser'=>$formUser->createView(), 
+            'formPassword'=>$formPassword->createView(),
+            'oldPasswordCheck'=>$oldPasswordCheck
+            ] 
+        );
+    }
+
 
     /**
      * @Route("/connexion", name = "security_login")
