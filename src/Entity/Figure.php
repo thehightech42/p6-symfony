54eb232152cca7a7a1c7cf507082bc3ca27d4037
Antonin PfistnerÃ <thehightech42@gmail.com>
2021-10-06 22:23:03 +0200

    ÃBugFix figure with same name

--- src/Entity/Figure.php
+++ src/Entity/Figure.php
@@ -6,9 +6,11 @@ use App\Repository\FigureRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=FigureRepository::class)
+ * @UniqueEntity("title", message = "Le titre de la figure dois être unique")
  */
 class Figure
 {
